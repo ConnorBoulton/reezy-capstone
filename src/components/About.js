8d151300c281b947c071f5e9da504b86639f17b2
Connor Boulton <cjboulton22@gmail.com>
2022-01-04 22:12:33 -0700

    about changes

--- src/components/About.js
+++ src/components/About.js
@@ -14,19 +14,47 @@ export default function About() {
         <span className="description">
           <h2>Capstone Project</h2>
           <p>
-            This is a project that I have been wanting to create for a while,
-            and there is no better time than during my DevMountain capstone
-            project! I've worked in many restaraunt kitchens across the Utah
-            valley, and have a always wanted an application that would store my
-            personal recipes. Not just some notebook or google document. So I
-            decided to create Reezy Recipes! A very simple app to help organize,
-            maintain, and store your recipes.
+            <br />
+            Before I came to DevMountain, I worked in the culinary industry
+            across four different restaurants in the Salt Lake valley and
+            graduated from culinary school at the Park City Culinary Institute.
+            After taking a long hard look at where my future was headed in terms
+            of financial security and sanity, I decided to leave the culinary
+            field for good. My three years of experience were enough for me to
+            discern that although I love to cook and the culture that surrounds
+            it, It was not a healthy field to be in. Long hours at the most
+            inconvenient time. Constantly in a rush and on a consistent
+            reoccurring deadline. All combined with low pay and high stress made
+            me look for an alternative to a new career path.
+            <br />
+            <br />I have always been interested in computers, or anything
+            science-related for that matter. During Christmas break of 2020, I
+            sat down with my two step-brothers who both worked in the computer
+            programming field. They enticed me with the idea of plunging myself
+            into the field with them. And showed me the attractive side and
+            benefits of coding. I followed through with their advice and
+            enrolled in the DevMountain coding boot camp program.
             <br />
             <br />
-            During my time at DevMountain, i've learned to create a full stack
-            application with a variety of methods and languages. For this
-            project I decide to use React for the front end, Express as the
-            server, and deployed with Heroku.
+            There I learned to be agile, and flexible in my learning skills. I
+            grasped the idea of how to think in a constructive and rudimentary
+            way. In the coding basics course, we had tackled everything from
+            methodologies to front-end web basics, python and data analytics,
+            and cryptography. In their specializations program for web dev, we
+            dove deep into React and learned how to build out a full-scale
+            application.
+            <br />
+            <br />
+            Towards the end of the 8 weeks of our specializations program, we
+            were tasked with creating our capstone project. I chose to create a
+            recipe app for my project, as it seemed applicable to my previous
+            circumstance. This is where Reezy Recipes was born. A simple app
+            that has access to over two million recipes and information
+            regarding those recipes. A basic yet effective way for finding the
+            right recipe for the individual. More features to come in the
+            future, but for now, I decide to leave it at that. If you are
+            reading this, thank you for looking at the project, and I hope you
+            enjoy!
           </p>
         </span>
       </div>
